2dc7ea5563befce369eb213004d3a811a709bf1d
elisabichiappini <elisabichiappini@gmail.com>
2023-11-21 17:08:57 +0100

    feat: mileston 1 e 2

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@
 'use strict';
 
 // MILESTONE 0 - Creare l’array di oggetti con le informazioni fornite
-// MILESTONE 1 - Stampare su console, per ogni membro del team, le informazioni membro, ruolo e image
+// MILESTONE 1 - Stampare su console, per ogni membro del team, le informazioni nome, ruolo e image
 // MILESTONE 2 - Stampare le stesse informazioni su DOM sottoforma di stringhe
 // BONUS 1 - Trasformare la stringa foto in una immagine effettiva
 // BONUS 2 - Organizzare i singoli membri in card/schede come immagine qui sotto
@@ -62,15 +62,51 @@ const membriTeam = [
 console.log(membriTeam);
 
 //m.1 attraversare l'array di oggetti
-const ulElemento = document.getElementsByName('grid-cards');
+const ulElemento = document.getElementById('grid-cards');
 
 for (let i = 0; i < membriTeam.length; i++) {
     const membriSignoli = membriTeam[i];
     console.log(`MEMBRO ${i + 1}`)
+
+    console.log(membriSignoli.nome);
 
     //ciclare l'oggetto
     for(let key in membriSignoli) {
         const detailsMembro = `${key}: ${membriSignoli[key]}`;
         console.log(detailsMembro);
     }
+
+    // soggetto
+    // cosa voglia
+    // dove
+    // ulElemento.innerHTML += `<li class="card">
+    //                             <img src="img/${membriSignoli.photo}">
+    //                             <h3>${membriSignoli.nome}</h3>
+    //                             <h4>${membriSignoli.role}</h4>
+    //                         </li>`;
+
+    // oppure
+
+    const li = document.createElement('li');
+    li.classList.add('card');
+
+    const img = document.createElement('img');
+    img.src = `img/${membriSignoli.photo}`;
+    li.append(img);
+    
+    const h3Elemento = document.createElement('h3');
+    h3Elemento.innerHTML = membriSignoli.nome;
+    li.append(h3Elemento);
+    
+    const h4Elemento = document.createElement('h4');
+    h4Elemento.innerHTML = membriSignoli.role;
+    li.append(h4Elemento);
+
+
+    ulElemento.append(li);
+
+
+    //TODO:FUNZIONE + BONUS 1, 2
+    
 }
+
