053917abe68a4862739101c08579386b6c0d4b5d
elisabichiappini <elisabichiappini@gmail.com>
2023-11-25 19:39:09 +0100

    feat: layout edit

---
+++ js/myscript.js
@@ -0,0 +1,123 @@
+/********************************************JS***********************************************/
+'use strict';
+
+// MILESTONE 0 - Creare l’array di oggetti con le informazioni fornite
+// MILESTONE 1 - Stampare su console, per ogni membro del team, le informazioni nome, ruolo e image
+// MILESTONE 2 - Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1 - Trasformare la stringa foto in una immagine effettiva
+// BONUS 2 - Organizzare i singoli membri in card/schede come immagine qui sotto
+/* 
+<li class="card">
+    <img src="img/wayne-barnett-founder-ceo.jpg" alt="image1">
+    <h3>Wayne Barnett</h3>
+    <h4>Founder & CEO</h4>
+</li>
+
+*/
+
+/*
+FUNCTIONS
+*/
+
+
+
+/*
+OPERATIONS
+*/
+
+const membriTeam = [
+    {
+    "nome" : 'Wayne Barnett',
+    "role" : 'Founder & Ceo',
+    "photo" : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {  
+    "nome" : 'Angela Caroll',
+    "role" : 'Chief Editor',
+    "photo" : 'angela-caroll-chief-editor.jpg'
+    },
+    {
+
+    "role" : 'Office Manager',
+    "photo" : 'walter-gordon-office-manager.jpg'
+    },
+    {
+    "nome" : 'Angela Lopez',
+    "role" : 'Social Media Manager',
+    "photo" : 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+    "nome" : 'Scott Estrada',
+    "role" : 'Developer',
+    "photo" : 'scott-estrada-developer.jpg'
+    },
+    {
+    "nome" : 'Barbara Ramos',
+    "role" : 'Graphic Designer',
+    "photo" : 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+//m.0
+console.log(membriTeam);
+
+//m.1 attraversare l'array di oggetti
+const ulElemento = document.getElementById('grid-cards');
+
+
+for (let i = 0; i < membriTeam.length; i++) {
+    const membriSignoli = membriTeam[i];
+    console.log(`MEMBRO ${i + 1}`)
+
+    console.log(membriSignoli.nome);
+
+    // soggetto
+    // cosa voglia
+    // dove
+
+    //ciclare l'oggetto
+    for(let key in membriSignoli) {
+        const detailsMembro = `${key}: ${membriSignoli[key]}`;
+        console.log(detailsMembro);
+    }
+
+    // elementi in dom con template litteral
+    // ulElemento.innerHTML += `<li class="card">
+    //                             <img src="img/${membriSignoli.photo}">
+    //                             <h3>${membriSignoli.nome}</h3>
+    //                             <h4>${membriSignoli.role}</h4>
+    //                         </li>`;
+
+    // oppure elementi in dom con il createElement:
+    const li = document.createElement('li');
+    li.classList.add('card');
+
+    const img = document.createElement('img');
+    img.src = `img/${membriSignoli.photo}`;
+    img.alt = membriSignoli.photo;
+    img.classList.add('image-set')
+    li.append(img);
+    
+    const h3Elemento = document.createElement('h3');
+
+    //gestire caso se non c'è il nome TO DOOOO
+    if (membriSignoli.nome !== undefined){
+        h3Elemento.textContent = membriSignoli.nome;
+    } else {
+        h3Elemento.textContent = 'nome non trovato';
+    }
+
+    h3Elemento.innerHTML = membriSignoli.nome;
+    li.append(h3Elemento);
+    
+    const h4Elemento = document.createElement('h4');
+    h4Elemento.innerHTML = membriSignoli.role;
+    li.append(h4Elemento);
+
+
+    ulElemento.append(li);
+
+
+    //TODO:FUNZIONE + BONUS 1, 2
+    
+}
